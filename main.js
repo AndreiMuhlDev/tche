6c5ecf9fd924e97c7654d8254ec033c34dc00df0
AndreiMuhlDev <86573787+AndreiMuhlDev@users.noreply.github.com>
2021-07-12 20:21:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ const swiper = new Swiper('.swiper-container', {
   mousewhell: true,
   keyboard: true
 })
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#carousel-img, #border,
+   #cards .card, #about .text,
+   .acting .map-acting, .txt-acting .title-acting,
+   .txt-acting .p-acting, #footer`,
+  { interval: 100 }
+)
